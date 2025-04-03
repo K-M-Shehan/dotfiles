a7894ce26470015f6c278ce7e3917e02ef272bf0
K-M-Shehan <miweshshehan@gmail.com>
2025-04-04 00:08:53 +0530

    added oil-nvim
    also changed the <leader>hv key binding to work with oil

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -1,3 +1,6 @@
 
 vim.g.mapleader = " ";
-vim.keymap.set("n", "<leader>hv", vim.cmd.Ex)
+-- got oil so this command is not used anymore
+--vim.keymap.set("n", "<leader>hv", vim.cmd.Ex)
+-- the new command set to the same key-binding
+vim.keymap.set('n', '<leader>hv', "<CMD>Oil --float<CR>", { desc = "Open parent directory"} )
