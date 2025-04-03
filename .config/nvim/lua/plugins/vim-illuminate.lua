104f96c6eed34579e7b62eee3233aad1491916c1
K-M-Shehan <miweshshehan@gmail.com>
2025-04-03 21:02:12 +0530

    fixed vim-illuminate

--- .config/nvim/lua/plugins/vim-illuminate.lua
+++ .config/nvim/lua/plugins/vim-illuminate.lua
@@ -1,7 +1,11 @@
 return {
-	"RRethy/vim-illuminate",
-	lazy = false,
-	config = function()
-		require("illuminate").configure({})
-	end,
+  "RRethy/vim-illuminate",
+  lazy = false,
+  config = function()
+    require("illuminate").configure({
+      providers = {
+        "lsp",
+      },
+    })
+  end,
 }
