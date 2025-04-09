62363eeaa847e68ad95b3fc0bf1b0dae45875916
K-M-Shehan <miweshshehan@gmail.com>
2025-04-09 19:05:38 +0530

    added keymap for lua

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -4,3 +4,6 @@ vim.g.mapleader = " ";
 --vim.keymap.set("n", "<leader>hv", vim.cmd.Ex)
 -- the new command set to the same key-binding
 vim.keymap.set('n', '<leader>hv', "<CMD>Oil --float<CR>", { desc = "Open parent directory"} )
+
+-- lua map
+vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
