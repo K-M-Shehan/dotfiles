a2eeda5e9c48780eab47f203cae19b293ae8d8e3
K-M-Shehan <miweshshehan@gmail.com>
2026-02-21 23:54:09 +0530

    add new keymaps
    also made rust analyzer the formater as well as lsp

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -8,37 +8,38 @@ return {
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-	  dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			local capabilities = vim.tbl_deep_extend( -- will compute capabilities once and reuse in handlers
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				require("cmp_nvim_lsp").default_capabilities()
+			)
 			require("mason-lspconfig").setup({
 				ensure_installed = {
-          "lua_ls",
-          "ts_ls",
-          "html",
-          "cssls",
-          "clangd",
-          "pyright",
-          "rust_analyzer",
-          "tailwindcss",
-          "gopls",
-          "csharp_ls"
-        },
+					"lua_ls",
+					"ts_ls",
+					"html",
+					"cssls",
+					"clangd",
+					"pyright",
+					"rust_analyzer",
+					"tailwindcss",
+					"gopls",
+					"csharp_ls",
+				},
 				handlers = {
-          -- default handler for any server without a specific handler
+					-- default handler for any server without a specific handler
 					function(server)
 						require("lspconfig")[server].setup({
-							capabilities = vim.tbl_deep_extend(
-								"force",
-								{},
-								vim.lsp.protocol.make_client_capabilities(),
-								require("cmp_nvim_lsp").default_capabilities()
-							),
+							capabilities = capabilities,
 						})
 					end,
+					-- lua ls
 					lua_ls = function()
 						require("lspconfig").lua_ls.setup({
 							settings = {
@@ -47,31 +48,36 @@ return {
 									workspace = { checkThirdParty = false },
 								},
 							},
-							capabilities = require("cmp_nvim_lsp").default_capabilities(),
+							capabilities = capabilities,
 						})
 					end,
+					-- typscript ls
 					ts_ls = function()
 						require("lspconfig").ts_ls.setup({
-							capabilities = require("cmp_nvim_lsp").default_capabilities(),
+							capabilities = capabilities,
 						})
 					end,
-          html = function()
-            require("lspconfig").html.setup({
-              capabilities = require("cmp_nvim_lsp").default_capabilities(),
-            })
-          end,
-          cssls = function()
-            require("lspconfig").cssls.setup({
-              capabilities = require("cmp_nvim_lsp").default_capabilities(),
-            })
-          end,
-          clangd = function ()
-            require("lspconfig").clangd.setup({
-              capabilities = require("cmp_nvim_lsp").default_capabilities(),
-              cmd = { "clangd", "--background-index" },
-            })
-          end,
-          pyright = function()
+					-- html ls
+					html = function()
+						require("lspconfig").html.setup({
+							capabilities = capabilities,
+						})
+					end,
+					--css ls
+					cssls = function()
+						require("lspconfig").cssls.setup({
+							capabilities = capabilities,
+						})
+					end,
+					-- c ls
+					clangd = function()
+						require("lspconfig").clangd.setup({
+							capabilities = capabilities,
+							cmd = { "clangd", "--background-index" },
+						})
+					end,
+					-- python ls
+					pyright = function()
 						require("lspconfig").pyright.setup({
 							settings = {
 								python = {
@@ -79,12 +85,13 @@ return {
 									venv = ".venv",
 								},
 							},
-							capabilities = require("cmp_nvim_lsp").default_capabilities(),
+							capabilities = capabilities,
 						})
 					end,
+					-- rust ls
 					rust_analyzer = function()
 						require("lspconfig").rust_analyzer.setup({
-							capabilities = require("cmp_nvim_lsp").default_capabilities(),
+							capabilities = capabilities,
 							settings = {
 								["rust-analyzer"] = {
 									checkOnSave = {
@@ -94,9 +101,10 @@ return {
 							},
 						})
 					end,
+					-- go ls
 					gopls = function()
 						require("lspconfig").gopls.setup({
-							capabilities = require("cmp_nvim_lsp").default_capabilities(),
+							capabilities = capabilities,
 							settings = {
 								gopls = {
 									analyses = {
@@ -107,14 +115,16 @@ return {
 							},
 						})
 					end,
+					-- c# ls
 					csharp_ls = function()
 						require("lspconfig").csharp_ls.setup({
-							capabilities = require("cmp_nvim_lsp").default_capabilities(),
+							capabilities = capabilities,
 						})
 					end,
+					-- tailwind ls
 					tailwindcss = function()
 						require("lspconfig").tailwindcss.setup({
-							capabilities = require("cmp_nvim_lsp").default_capabilities(),
+							capabilities = capabilities,
 							filetypes = {
 								"html",
 								"css",
@@ -138,23 +148,47 @@ return {
 		config = function()
 			-- LSP UI tweaks
 			vim.diagnostic.config({
-        float = { border = "rounded" },
-        virtual_text = true,
-        signs = true,
-        update_in_insert = true,
-      })
+				float = { border = "rounded" },
+				virtual_text = true,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = " ",
+						[vim.diagnostic.severity.WARN] = " ",
+						[vim.diagnostic.severity.HINT] = " ",
+						[vim.diagnostic.severity.INFO] = " ",
+					},
+				},
+				update_in_insert = true,
+			})
 			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 
 			-- Scoped keymaps
 			vim.api.nvim_create_autocmd("LspAttach", {
 				callback = function(event)
-					local opts = { buffer = event.buf }
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+					local opts = function(desc)
+						return { buffer = event.buf, desc = desc }
+					end
+
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Hover docs"))
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Go to implementation"))
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts("References"))
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts("Rename"))
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
+					vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts("Show diagnostic"))
+          -- only format through null ls to avoid conflicts
 					vim.keymap.set("n", "<leader>gf", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
+						vim.lsp.buf.format({
+							async = true,
+							filter = function(client)
+                local filetype = vim.bo[event.buf].filetype
+                if filetype == "rust" then
+                  return client.name == "rust_analyzer"
+                end
+								return client.name == "null-ls"
+							end,
+						})
+					end, opts("Format file"))
 				end,
 			})
 		end,
